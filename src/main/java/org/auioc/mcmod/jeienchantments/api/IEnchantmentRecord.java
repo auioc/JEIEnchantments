8d1bc4a65d6819463402ca6d10db3de33a0d446e
WakelessSloth56 <wsloth56@foxmail.com>
2023-02-07 22:39:04 +0800

    feat(api): IEnchantmentRecord

---
+++ src/main/java/org/auioc/mcmod/jeienchantments/api/IEnchantmentRecord.java
@@ -0,0 +1,12 @@
+package org.auioc.mcmod.jeienchantments.api;
+
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public interface IEnchantmentRecord {
+
+    Enchantment enchantment();
+
+}
